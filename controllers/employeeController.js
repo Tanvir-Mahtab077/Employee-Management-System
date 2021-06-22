a75826413fab65dad064bb46ce60749dce68ef3e
Tanvir-Mahtab077 <shovontanvir123@gmail.com>
2021-06-23 00:57:56 +0600

    Employee update delete one employee and delete all employee api added

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -19,12 +19,65 @@ let employeeList= function(req,res){
             // console.log(employees)
             res.status(httpStatus.OK).json(employees)
         })
+        .catch(error=>{
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+        })
 
 }
+
+let employeeUpdate= function(req,res){
+    let {id}=req.params
+    let {name,status,selary,phone,email}=req.body
+    employeeModel.update({
+        name,status,selary,phone,email
+    },
+    {
+        where:{
+            id:id
+        }
+    })
+    .then(()=>{
+        res.status(httpStatus.OK).json({message:"Employee Updated Successfully"})
+    })
+    .catch(error=>{
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+    })
+
+}
+
+employeeDelete= function(req,res){
+    let {id}=req.params
+   employeeModel.destroy({
+      where:{
+          id
+      }
+   })
+   .then(()=>{
+       res.status(httpStatus.OK).json({message:"Employee Deleted Succesfully"})
+   })
+   .catch(error=>{
+       res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+   })
+}
+ let employeeALLDelete= function(req,res){
+    employeeModel.destroy({
+        where:{}
+    })
+    .then(()=>{
+        res.status(httpStatus.OK).json({message:"All Records From Employee is Deleted Succesfully"})
+    })
+    .catch(error=>{
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+    })
+ }
+
 
 
 
 module.exports={
     employeeCreate,
-    employeeList
+    employeeList,
+    employeeUpdate,
+    employeeDelete,
+    employeeALLDelete
 }
--- routes/employee.js
+++ routes/employee.js
@@ -3,6 +3,10 @@ let employeeController=require('../controllers/employeeController')
 
 router.post('/create',employeeController.employeeCreate)
 router.get('/list',employeeController.employeeList)
+router.patch('/update/:id',employeeController.employeeUpdate)
+router.delete('/deleteOne/:id',employeeController.employeeDelete)
+router.delete('/deleteAll',employeeController.employeeALLDelete)
+
 
 
 
